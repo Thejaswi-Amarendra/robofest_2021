70193c78abfe0245e20b8974ecb56dcdd9ec8447
Thejaswi-Amarendra <79244679+Thejaswi-Amarendra@users.noreply.github.com>
2021-10-29 21:11:59 +0530

    Create task2.py

---
+++ task2/task2.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+im = cv2.imread("task2.jfif", 1)
+np.shape(im)
+y = np.shape(im)[0]//4
+x = np.shape(im)[1]//4
+col_dict = {"yellow":[255,255,0], "green":[0, 128, 1], "white":[255, 255, 255], "blue": [0, 0, 254], "red": [254, 0, 0], "orange":[254, 165, 0]}    #Dictionary containing name and corresponding RGB values.(Sufficient only for this picture)
+
+def color(lst, dct):                #This function finds the name of the color closest to the RGB value obtained from the image.
+    minimum = 10000
+    for i in range(len(dct)):
+        d = abs(lst[0]-list(dct.values())[i][0]) + abs(lst[1]- list(dct.values())[i][1])+ abs(lst[2]- list(dct.values())[i][2])
+        if d<=minimum:
+            minimum = d
+            name = list(dct.keys())[i]
+    return name
+
+for i in range(1,4):
+    for j in range(1,4):
+        rgb = list(im[y*i][x*j])[::-1]
+        string = color(rgb, col_dict)
+        coord = (y*j + (j-2)*40 - 40, x*i + (i-2)*40)   #Formula to normalise the coordinates so that text is printed at the center of the colored box
+        cv2.putText(im, string, coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2) #Text not visible for the center box coz its red
+        j=j+1
+    i=i+1
+
+cv2.imshow("image", im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
